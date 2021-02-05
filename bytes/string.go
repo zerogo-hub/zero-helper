e399cac9bea0cc9bb45f5e9ee8006785c9c39c7a
alex <alex_my@126.com>
2021-02-05 21:39:18 +0800

    StringToByte重命名为StringToByte

--- bytes/string.go
+++ bytes/string.go
@@ -8,7 +8,7 @@ import (
 // StringToByte 字符串转 []byte，转出的 []byte 可读不可写
 //
 // 来源: https://www.toutiao.com/a6918883127146349067/
-func StringToByte(s string) []byte {
+func StringToBytes(s string) []byte {
 	l := len(s)
 	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
 		Data: (*(*reflect.StringHeader)(unsafe.Pointer(&s))).Data,
