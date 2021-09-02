cf5e3fc43b1f3fcc061f5196c45290212639398b
alex <alex_my@126.com>
2021-09-02 23:47:58 +0800

    添加字母大小写切换

--- bytes/bytes_test.go
+++ bytes/bytes_test.go
@@ -39,3 +39,71 @@ func TestInt(t *testing.T) {
 		t.Error("uint64 error")
 	}
 }
+
+func TestStringToBytes(t *testing.T) {
+	s := "hello world"
+
+	b1 := zerobytes.StringToBytes(s)
+	b2 := []byte(s)
+
+	if len(b1) != len(b2) {
+		t.Error("TestStringToBytes error")
+		return
+	}
+
+	for idx, b10 := range b1 {
+		b20 := b2[idx]
+		if b10 != b20 {
+			t.Error("TestStringToBytes not the same")
+			break
+		}
+	}
+}
+
+func TestCharLower(t *testing.T) {
+	if zerobytes.CharLower('A') != 'a' {
+		t.Error("CharLower A error")
+	}
+
+	if zerobytes.CharLower('Z') != 'z' {
+		t.Error("CharLower Z error")
+	}
+
+	if zerobytes.CharLower(1) != 1 {
+		t.Error("CharLower 1 error")
+	}
+}
+
+func TestCharUpper(t *testing.T) {
+	if zerobytes.CharUpper('a') != 'A' {
+		t.Error("CharLower a error")
+	}
+
+	if zerobytes.CharUpper('z') != 'Z' {
+		t.Error("CharLower z error")
+	}
+
+	if zerobytes.CharUpper(1) != 1 {
+		t.Error("CharLower 1 error")
+	}
+}
+
+func TestFirstLower(t *testing.T) {
+	if zerobytes.FirstLower("Hello world") != "hello world" {
+		t.Error("FirstLower error")
+	}
+
+	if zerobytes.FirstLower("") != "" {
+		t.Error("FirstLower empty error")
+	}
+}
+
+func TestFirstUpper(t *testing.T) {
+	if zerobytes.FirstUpper("hello world") != "Hello world" {
+		t.Error("FirstUpper error")
+	}
+
+	if zerobytes.FirstUpper("") != "" {
+		t.Error("FirstUpper empty error")
+	}
+}
--- bytes/string.go
+++ bytes/string.go
@@ -2,6 +2,7 @@ package bytes
 
 import (
 	"reflect"
+	"strings"
 	"unsafe"
 )
 
@@ -15,4 +16,40 @@ func StringToBytes(s string) []byte {
 		Len:  l,
 		Cap:  l,
 	}))
+}
+
+// CharUpper 转大写 a -> A
+func CharUpper(char byte) byte {
+	if char >= 'a' && char <= 'z' {
+		return char - 32
+	}
+
+	return char
+}
+
+// CharLower 转小写 A -> a
+func CharLower(char byte) byte {
+	if char >= 'A' && char <= 'Z' {
+		return char + 32
+	}
+
+	return char
+}
+
+// FirstUpper 首字母大写
+func FirstUpper(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+// FirstLower 首字母小写
+func FirstLower(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	return strings.ToLower(s[:1]) + s[1:]
 }
