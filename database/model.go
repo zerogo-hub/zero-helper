3b0ec55c60b5c43f09ec429b86cf415d3e4c733c
alex <alex_my@126.com>
2021-09-07 22:40:52 +0800

    修复软删除不成功的问题

--- database/model.go
+++ database/model.go
@@ -1,11 +1,15 @@
 package database
 
+import (
+	"gorm.io/gorm"
+)
+
 // Model 替代 gorm.Model，无 id，需要自定义主键
 // 时间为 秒，
 type Model struct {
-	CreatedAt int64  `gorm:"comment:'记录创建时间'"`
-	UpdatedAt int64  `gorm:"comment:'最后一次更新时间'"`
-	DeletedAt *int64 `gorm:"comment:'软删除时间'"`
+	CreatedAt int64          `gorm:"comment:'记录创建时间'"`
+	UpdatedAt int64          `gorm:"comment:'最后一次更新时间'"`
+	DeletedAt gorm.DeletedAt `gorm:"index;comment:'软删除时间'"`
 }
 
 // ModelID 替代 gorm.Model，带自增 id
