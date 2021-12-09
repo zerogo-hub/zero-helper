4f96db0a8735d43cf81e0f7c13009da3fd2bfcd1
alex <alex_my@126.com>
2021-12-09 17:41:16 +0800

    ✨ feat(database): 添加ModelNoJSON

--- database/model.go
+++ database/model.go
@@ -12,6 +12,13 @@ type Model struct {
 	DeletedAt gorm.DeletedAt `gorm:"index;comment:'软删除时间'" json:"deletedAt"`
 }
 
+// ModelNoJSON 同 Model
+type ModelNoJSON struct {
+	CreatedAt int64          `gorm:"comment:'记录创建时间'" json:"-"`
+	UpdatedAt int64          `gorm:"comment:'最后一次更新时间'" json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index;comment:'软删除时间'" json:"-"`
+}
+
 // ModelID 替代 gorm.Model，带自增 id
 type ModelID struct {
 	ID uint `gorm:"primaryKey;AUTO_INCREMENT" json:"id"`
