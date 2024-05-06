edc860b4d4c6183d73b59ef07f46ba2af22e0325
alex <alex_my@126.com>
2024-05-06 14:53:42 +0800

    新增摘要算法

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -36,6 +36,13 @@ func Md5Byte(b []byte) string {
 	return hex.EncodeToString(m.Sum(nil))
 }
 
+// Md5ByteToByte 对指定内容生成信息摘要
+func Md5ByteToByte(b []byte) []byte {
+	m := md5.New()
+	m.Write(b)
+	return m.Sum(nil)
+}
+
 // Sha1 对指定内容生成信息摘要
 //
 // eg: Sha1("123456") -> 7c4a8d09ca3762af61e59520943dc26494f8941b
@@ -50,6 +57,13 @@ func Sha1Byte(b []byte) string {
 	s := sha1.New()
 	s.Write(b)
 	return hex.EncodeToString(s.Sum(nil))
+}
+
+// Sha1ByteToByte 对指定内容生成信息摘要
+func Sha1ByteToByte(b []byte) []byte {
+	s := sha1.New()
+	s.Write(b)
+	return s.Sum(nil)
 }
 
 // Sha224 对指定内容生成信息摘要
@@ -68,6 +82,13 @@ func Sha224Byte(b []byte) string {
 	return hex.EncodeToString(s.Sum(nil))
 }
 
+// Sha224ByteToByte 对指定内容生成信息摘要
+func Sha224ByteToByte(b []byte) []byte {
+	s := sha256.New224()
+	s.Write(b)
+	return s.Sum(nil)
+}
+
 // Sha256 对指定内容生成信息摘要
 //
 // eg: Sha256("123456") -> 8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
@@ -84,11 +105,19 @@ func Sha256Byte(b []byte) string {
 	return hex.EncodeToString(s.Sum(nil))
 }
 
+// Sha256ByteToByte 对指定内容生成信息摘要
+func Sha256ByteToByte(b []byte) []byte {
+	s := sha256.New()
+	s.Write(b)
+	return s.Sum(nil)
+}
+
 // Sha384 对指定内容生成信息摘要
 //
 // eg: Sha384("123456")
-// 	-> 0a989ebc4a77b56a6e2bb7b19d995d185ce44090c13e2984b7ecc6d446d4b61ea9991b76a4c2f04b1
-//     b4d244841449454
+//
+//		-> 0a989ebc4a77b56a6e2bb7b19d995d185ce44090c13e2984b7ecc6d446d4b61ea9991b76a4c2f04b1
+//	    b4d244841449454
 func Sha384(str string) string {
 	s := sha512.New384()
 	s.Write(zerobytes.StringToBytes(str))
@@ -102,11 +131,19 @@ func Sha384Byte(b []byte) string {
 	return hex.EncodeToString(s.Sum(nil))
 }
 
+// Sha384ByteToByte 对指定内容生成信息摘要
+func Sha384ByteToByte(b []byte) []byte {
+	s := sha512.New384()
+	s.Write(b)
+	return s.Sum(nil)
+}
+
 // Sha512 对指定内容生成信息摘要
 //
 // eg: Sha512("123456")
-// 	-> ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c
-// 	   17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413
+//
+//	-> ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c
+//	   17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413
 func Sha512(str string) string {
 	s := sha512.New()
 	s.Write(zerobytes.StringToBytes(str))
@@ -120,10 +157,18 @@ func Sha512Byte(b []byte) string {
 	return hex.EncodeToString(s.Sum(nil))
 }
 
+// Sha512ByteToByte 对指定内容生成信息摘要
+func Sha512ByteToByte(b []byte) []byte {
+	s := sha512.New()
+	s.Write(b)
+	return s.Sum(nil)
+}
+
 // HmacMd5 对指定内容生成信息摘要
 //
 // eg: HmacMd5("123456", "abcdef")
-// 	-> c6bdcc80c381536a3e85f2ee5f71cebb
+//
+//	-> c6bdcc80c381536a3e85f2ee5f71cebb
 func HmacMd5(str, key string) string {
 	mac := hmac.New(md5.New, zerobytes.StringToBytes(key))
 	mac.Write(zerobytes.StringToBytes(str))
@@ -137,9 +182,17 @@ func HmacMd5Byte(b, key []byte) string {
 	return hex.EncodeToString(mac.Sum(nil))
 }
 
+// HmacMd5ByteToByte 对指定内容生成信息摘要
+func HmacMd5ByteToByte(b, key []byte) []byte {
+	mac := hmac.New(md5.New, key)
+	mac.Write(b)
+	return mac.Sum(nil)
+}
+
 // HmacSha1 对指定内容生成信息摘要
 //
 // eg: HmacSha1("123456", "abcdef")
+//
 //	-> b8466fbb9634771d25d8ddd1242484bdb748b179
 func HmacSha1(str, key string) string {
 	mac := hmac.New(sha1.New, zerobytes.StringToBytes(key))
@@ -154,9 +207,17 @@ func HmacSha1Byte(b, key []byte) string {
 	return hex.EncodeToString(mac.Sum(nil))
 }
 
+// HmacSha1ByteToByte 对指定内容生成信息摘要
+func HmacSha1ByteToByte(b, key []byte) []byte {
+	mac := hmac.New(sha1.New, key)
+	mac.Write(b)
+	return mac.Sum(nil)
+}
+
 // HmacSha256 对指定内容生成信息摘要
 //
 // eg: HmacSha256("123456", "abcdef")
+//
 //	-> ec4a11a5568e5cfdb5fbfe7152e8920d7bad864a0645c57fe49046a3e81ec91d
 func HmacSha256(str, key string) string {
 	mac := hmac.New(sha256.New, zerobytes.StringToBytes(key))
@@ -171,11 +232,19 @@ func HmacSha256Byte(b, key []byte) string {
 	return hex.EncodeToString(mac.Sum(nil))
 }
 
+// HmacSha256ByteToByte 对指定内容生成信息摘要
+func HmacSha256ByteToByte(b, key []byte) []byte {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(b)
+	return mac.Sum(nil)
+}
+
 // HmacSha512 对指定内容生成信息摘要
 //
 // eg: HmacSha512("123456", "abcdef")
+//
 //	-> 130a4caafb11b798dd7528628d21f742feaad266e66141cc2ac003f0e6437cb57
-// 		49245af8a3018d354e4b55e14703a5966808438afe4aae516d2824b014b5902
+//		49245af8a3018d354e4b55e14703a5966808438afe4aae516d2824b014b5902
 func HmacSha512(str, key string) string {
 	mac := hmac.New(sha512.New, zerobytes.StringToBytes(key))
 	mac.Write(zerobytes.StringToBytes(str))
@@ -189,12 +258,20 @@ func HmacSha512Byte(b, key []byte) string {
 	return hex.EncodeToString(mac.Sum(nil))
 }
 
+// HmacSha512ByteToByte 对指定内容生成信息摘要
+func HmacSha512ByteToByte(b, key []byte) []byte {
+	mac := hmac.New(sha512.New, key)
+	mac.Write(b)
+	return mac.Sum(nil)
+}
+
 // URLEncode ..
 //
 // 相当于 JS encodeURIComponent
 //
 // eg: URLEncode("www.keylala.cn?name=alex&age=18&say=你好")
-//  -> "www.keylala.cn%3Fname%3Dalex%26age%3D18%26say%3D%E4%BD%A0%E5%A5%BD"
+//
+//	-> "www.keylala.cn%3Fname%3Dalex%26age%3D18%26say%3D%E4%BD%A0%E5%A5%BD"
 func URLEncode(str string) string {
 	return url.QueryEscape(str)
 }
@@ -204,7 +281,8 @@ func URLEncode(str string) string {
 // 相当于 JS decodeURIComponent
 //
 // eg: URLDecode(www.keylala.cn%3Fname%3Dalex%26age%3D18%26say%3D%E4%BD%A0%E5%A5%BD)
-//  -> "www.keylala.cn?name=alex&age=18&say=你好"
+//
+//	-> "www.keylala.cn?name=alex&age=18&say=你好"
 func URLDecode(str string) string {
 	data, err := url.QueryUnescape(str)
 	if err != nil {
@@ -216,7 +294,8 @@ func URLDecode(str string) string {
 // Base64Encode base64 编码
 //
 // eg: Base64Encode("https://www.keylala.cn/json?str=hello world")
-// 	-> aHR0cHM6Ly93d3cua2V5bGFsYS5jbi9qc29uP3N0cj1oZWxsbyB3b3JsZA==
+//
+//	-> aHR0cHM6Ly93d3cua2V5bGFsYS5jbi9qc29uP3N0cj1oZWxsbyB3b3JsZA==
 func Base64Encode(str string) string {
 	return Base64EncodeByte(zerobytes.StringToBytes(str))
 }
